03303e8d41c80ae9300065bdac1153412bdd8005
threefoureight <bl4818@gmail.com>
2024-01-19 16:39:48 -0500

    Initial commit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -42,6 +42,17 @@ const menuItems = [
 ]
 
 function Menu() {
+
+  const [purchasedItems, setPurchasedItems] = React.useState([])
+
+  const addPurchasedItems = purchasedItem => {
+    setPurchasedItems([ ... purchasedItems, purchasedItem])
+  }
+
+  const removePurchasedItem = name => {
+    setPurchasedItems(purchasedItems.filter(item => name != item.name))
+  }
+
   return (
     <section>
       <dl>
@@ -51,11 +62,13 @@ function Menu() {
               name={menuItem.name}
               price={menuItem.price}
               picture={menuItem.picture}
+              addPurchasedItems={addPurchasedItems}
+              removePurchasedItem={removePurchasedItem}
             />
           )
         })}
       </dl>
-      <Receipt purchasedItems={[]} />
+      <Receipt purchasedItems={purchasedItems} />
     </section>
   )
 }
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,10 +1,32 @@
 import React from 'react'
 
-const MenuItem = props => (
-  <button class="menu-item">
+const MenuItem = props => {
+  const [selected, setSelected] = React.useState(false);
+
+  const toggleSelected = () => {
+    if (!selected) {
+    props.addPurchasedItems({
+      name: props.name,
+      price: props.price,
+    })
+  }
+
+    else {
+      props.removePurchasedItem(props.name)
+    }
+
+    setSelected(!selected)
+  }
+  
+
+return (
+  <button class={selected ? "menu-item selected": "menu-item"} onClick={toggleSelected}>
     <img src={props.picture} alt={props.name} />
     <dt>{props.name}</dt>
     <dd>{props.price}</dd>
+    UseState 
   </button>
 )
+
+}
 export default MenuItem
